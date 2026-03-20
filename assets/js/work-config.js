0aca896452eb4b69edad952a7bfb11c942ec074d
Munajt <60009613+czoPredato@users.noreply.github.com>
2026-03-20 07:56:01 +0100

    Update work-config.js

--- assets/js/work-config.js
+++ assets/js/work-config.js
@@ -12,20 +12,20 @@ var WORK_CATEGORIES = [
 		label: 'Posters',
 		folder: 'posters',
 		
-		images: ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg']
+		images: ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg']
 	},
 	{
 		slug: 'business-cards',
 		label: 'Business Cards',
 		folder: 'business-cards',
 		
-		images: ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg']
+		images: ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg']
 	},
 	{
 		slug: 'thumbnails',
 		label: 'Thumbnails',
 		folder: 'thumbnails',
 		
-		images: ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg']
+		images: ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg']
 	}
 ];
