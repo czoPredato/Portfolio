2b60ec2a91b5c2ac2ec268137ce015f3460ac6b0
Munajt <60009613+czoPredato@users.noreply.github.com>
2026-03-21 20:19:58 +0100

    Update work-config.js

--- assets/js/work-config.js
+++ assets/js/work-config.js
@@ -12,7 +12,7 @@ var WORK_CATEGORIES = [
 		label: 'Posters',
 		folder: 'posters',
 		
-		images: ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg']
+		images: ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg']
 	},
 	{
 		slug: 'business-cards',
